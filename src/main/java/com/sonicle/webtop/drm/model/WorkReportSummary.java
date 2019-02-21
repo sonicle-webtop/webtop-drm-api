fc297c346b564fdaffe6b299355df745cdc513c5
Alessandro Vasi <alessandro.vasi@encodata.it>
2019-02-21 17:03:09 +0100

    WTDRM-1 WorkReport Summary

---
+++ src/main/java/com/sonicle/webtop/drm/model/WorkReportSummary.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2017 Sonicle S.r.l.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation with the addition of the following permission
+ * added to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED
+ * WORK IN WHICH THE COPYRIGHT IS OWNED BY SONICLE, SONICLE DISCLAIMS THE
+ * WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * You can contact Sonicle S.r.l. at email address sonicle[at]sonicle[dot]com
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * Sonicle logo and Sonicle copyright notice. If the display of the logo is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Copyright (C) 2017 Sonicle S.r.l.".
+ */
+package com.sonicle.webtop.drm.model;
+
+import com.sonicle.webtop.core.sdk.UserProfileId;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ *
+ * @author lssndrvs
+ */
+public class WorkReportSummary {
+	
+	private Integer duration;
+	private String description;
+	private String operatorId;
+	private String customerId;
+	private Integer companyId;
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getOperatorId() {
+		return operatorId;
+	}
+
+	public void setOperatorId(String operatorId) {
+		this.operatorId = operatorId;
+	}
+
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public Integer getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(Integer companyId) {
+		this.companyId = companyId;
+	}
+	
+	
+	public UserProfileId getOperatorProfileId(String domainId) {
+		return new UserProfileId(domainId, getOperatorId());
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+				.append(getDuration())
+				.append(getDescription())
+				.append(getOperatorId())
+				.append(getCustomerId())
+				.append(getCompanyId())
+				.toHashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof WorkReportSummary == false) {
+			return false;
+		}
+		if (this == obj) {
+			return true;
+		}
+		final WorkReportSummary otherObject = (WorkReportSummary) obj;
+		return new EqualsBuilder()
+				.append(getDuration(), otherObject.getDuration())
+				.append(getDescription(), otherObject.getDescription())
+				.append(getOperatorId(), otherObject.getOperatorId())
+				.append(getCustomerId(), otherObject.getCustomerId())
+				.append(getCompanyId(), otherObject.getCompanyId())
+				.isEquals();
+	}
+	
+}
